006b34897036508df92524641f0280b66584d8fc
Olugbenga Ajayi <gbejula@gmail.com>
2021-04-12 07:18:40 +0100

    centered heading picture

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,13 +6,19 @@ const HomeScreen = () => {
   return (
     <div>
       <Container>
-        <Row>
-          <Col lg={8} md={6} sm={1}>
-            <div>
-              <img className='pic' src={gbenga} alt='gbenga' />
-            </div>
+        <Row className='justify-content-center'>
+          <Col
+            lg={8}
+            md={6}
+            sm={1}
+            className='justify-content-center align-items-center'>
+            <img className='pic' src={gbenga} alt='gbenga' />
           </Col>
-          <Col className='text-center mt-4' lg={12} md={6} sm={1}>
+          <Col
+            className='text-center mt-4 justify-content-center align-items-center'
+            lg={12}
+            md={6}
+            sm={1}>
             <h1>Hi I'm Gbenga,</h1>
             <h1>Full-stack Developer</h1>
           </Col>
