9c14b4cfc899612a9a9c9eb9f0d3e91f9dfe3535
Olugbenga Ajayi <gbejula@gmail.com>
2021-08-17 23:38:37 +0100

    added content to about page

--- src/screens/About.js
+++ src/screens/About.js
@@ -10,6 +10,18 @@ const About = () => {
         support engineer. During this period, I gained experience in people
         management and excellent customer science while working with different
         people in different roles.
+        <br /> <br />
+        After realizing my passion for software development, I took courses from
+        various online site like coursera, freecodecamp and udacity. Afterwards,
+        I was opportuned to be part of a 3 months immersive program by Facebook
+        called DevC Training in collaboration with Andela learning community
+        (ALC). The program taught basic to immediate skills of a full-stack
+        developer. Furthermore, I went on to learn about ReactJs in other to
+        build exceptional beautiful user-interfaces.
+        <br /> <br />I am presently looking to land my first job as a frontend
+        developer in an organization that fosters growth, learning and career
+        progression. I also look to contribute immersely to this organization
+        through my education, previous skills and acquired knowledge.
       </p>
     </div>
   );
