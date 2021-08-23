de727c9d61ae4b881ba158568e3982af0f5fa2c9
Olugbenga Ajayi <gbejula@gmail.com>
2021-08-23 23:51:51 +0100

    added context to contact page

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <>
-      <h1>Contact</h1>
-    </>
+    <div className='greeting-container'>
+      <h1 className='greeting'>Hello</h1>
+      <span className='greeting-text'>
+        I would like to hear from you. We can also work together.
+      </span>
+
+      <div>
+        <i className='far fa-2x fa-envelope fa-icon'></i>
+        <p className='info'>gbejula@gmail.com</p>
+      </div>
+      <div>
+        <i className='fas fa-2x fa-mobile-alt fa-icon'></i>
+        <p className='info'>+2347032626115</p>
+      </div>
+    </div>
   );
 };
 
