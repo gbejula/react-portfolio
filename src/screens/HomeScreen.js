bc327fe4d785fca8328c1812ee150e27c8196b57
Olugbenga Ajayi <gbejula@gmail.com>
2021-08-29 00:31:26 +0100

    changed color and info on home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -20,7 +20,7 @@ const HomeScreen = () => {
       <Container>
         <Row className='row'>
           <Col xs={12} md={8} className='text'>
-            <h1 className='title'>Hi, I'm Gbenga, a Full-stack Developer.</h1>
+            <h1 className='title'>Hi, I'm Gbenga, a Full Stack Developer.</h1>
             <p className='message'>
               I create responsive web applications with an eye for beautiful
               user interface and experience,{' '}
